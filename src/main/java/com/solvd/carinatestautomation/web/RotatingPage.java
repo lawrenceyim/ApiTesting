201ea4f05c00704ebe118d6374745174f1b10d61
lawrenceyim <lawrence.yim.010@gmail.com>
2024-02-09 10:56:41 -0500

    Added FindBy tag for header in RotatingPage

--- src/main/java/com/solvd/carinatestautomation/web/RotatingPage.java
+++ src/main/java/com/solvd/carinatestautomation/web/RotatingPage.java
@@ -4,8 +4,10 @@ import com.solvd.carinatestautomation.web.components.Header;
 import com.zebrunner.carina.utils.config.Configuration;
 import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.FindBy;
 
 public class RotatingPage extends AbstractPage {
+    @FindBy(id = "header")
     private Header header;
 
     public RotatingPage(WebDriver driver) {
